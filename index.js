3396a4ee77313cfd9c3c97e526fc68c706d4a155
pavan <Pavanpk123@gmail.com>
2022-09-01 17:01:40 +0530

    reduce

---
+++ index.js
@@ -0,0 +1,10 @@
+const numbers = [175,50,25];
+const number1 = numbers.reduce((i,k)=>i+k);
+console.log(number1)
+
+
+// substract values reduce
+
+const numbers = [1750,45,670,756,4345];
+const number2 = numbers.reduce((pavan,gvs)=>pavan-gvs);
+console.log(number2)
